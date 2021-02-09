5495c503631fc8f4085b1e62c801d846d1e0d58a
AUTISM81 <christian.backstrom@elev.ga.ntig.se>
2021-02-09 11:08:35 +0100

    feat: added login

--- routes/login.js
+++ routes/login.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('form', {title: 'Schoolsoft'});
 });
 
+/* post login */
+router.post('/', function(req, res, next) {
+
+    console.log(req.body);
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    //logga in med DOLD tvåFaktorLogin
+    if (password == "bajs"){
+        res.render('form', {title:'du vann'});
+    }else {
+        res.render('form', {title: 'Schoolsoft'}, {desc: 'wrong username or password'});
+    }
+});
+
 module.exports = router;
