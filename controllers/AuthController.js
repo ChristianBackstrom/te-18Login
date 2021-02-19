458ce75174967fada17618fd2cd7e6c947d610b5
AUTISM81 <christian.backstrom@elev.ga.ntig.se>
2021-02-19 10:03:45 +0100

    feat: controller added

---
+++ controllers/AuthController.js
@@ -0,0 +1,48 @@
+const bcrypt = require('bcrypt');
+const { query } = require('../models/db');
+const { body, validationResult } = require('express-validator');
+const { render } = require('../app');
+
+module.exports.show = async function(req, res, next) {
+    //logiken för att visa login form
+        res.render('form', {title: 'Schoolsoft'});
+};
+
+module.exports.store = async function(req, res, next) {
+    //logiken för att logga in användaren
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.render('form',{ errors: errors.array() });
+    }
+
+    console.log(req.body);
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    try{
+        const sql = 'SELECT password FROM users WHERE name = ?';
+        const result = await query(sql, username);
+        // Load hash from your password DB.
+        if(result.length > 0){
+            bcrypt.compare(password, result[0].password, function(err, result) {
+                if (result == true){
+                    req.session.loggedin = true;
+                    req.session.username = username;
+                    res.redirect('/home');
+                } else {
+                    res.render('form', {
+                        error: 'Username or password is invalid',
+                        msg: ''
+                    });
+                }
+            });
+        } else {
+            res.render('form', {error: 'Username or password is invalid', msg: ''});
+        }
+    } catch (e) {
+        next(e);
+        console.error(e);
+    }
+};
--- routes/login.js
+++ routes/login.js
@@ -1,71 +1,17 @@
 const express = require('express');
+const { body } = require('express-validator');
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const { query } = require('../models/db');
-const { body, validationResult } = require('express-validator');
 
-
-
+const AuthController = require('../controllers/AuthController');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('form', {title: 'Schoolsoft'});
-});
-
-/* B-crypt */
-router.get('/kryptan/:pwd', function(req, res, next) {
-
-    const myPlaintextPassword = req.params.pwd;
-
-    bcrypt.hash(myPlaintextPassword, 1, function(err, hash) {
-        // Store hash in your password DB.
-        res.json({
-            pwd: hash
-        });
-    });
-});
+router.get('/', AuthController.show);
 
 /* post login */
 router.post('/', 
-    body('username').notEmpty().trim(), 
-    body('password').notEmpty(), 
-    async function(req, res, next) {
-
-    // Finds the validation errors in this request and wraps them in an object with handy functions
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      res.render('form',{ errors: errors.array() });
-    }
-
-    console.log(req.body);
-
-    const username = req.body.username;
-    const password = req.body.password;
-
-    try{
-        const sql = 'SELECT password FROM users WHERE name = ?';
-        const result = await query(sql, username);
-        // Load hash from your password DB.
-        if(result.length > 0){
-            bcrypt.compare(password, result[0].password, function(err, result) {
-                if (result == true){
-                    req.session.loggedin = true;
-                    req.session.username = username;
-                    res.redirect('/home');
-                } else {
-                    res.render('form', {
-                        error: 'Username or password is invalid',
-                        msg: ''
-                    });
-                }
-            });
-        } else {
-            res.render('form', {error: 'Username or password is invalid', msg: ''});
-        }
-    } catch (e) {
-        next(e);
-        console.error(e);
-    }
-});
+    body('username').notEmpty().trim(),
+    body('password').notEmpty(),
+    AuthController.store
+);
 
 module.exports = router;
